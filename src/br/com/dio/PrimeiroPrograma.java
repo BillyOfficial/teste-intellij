8dd178e2d36e6fddc8be1b5bbd1b51750bb2fed2
BillyOfficial <arthurferreira183@hotmail.com>
2023-08-15 19:08:23 -0300

    Agora testando commit/push com modificações

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -6,10 +6,46 @@ public class PrimeiroPrograma {
     public static void main(String[] args) {
         Gato gato = new Gato();
         System.out.println(gato);
+
+        Livro livro1 = new Livro("Pequeno Príncipe", 300);
+        System.out.println(livro1);
         /*
         int primeiro = 15;
         int segundo = 10;
         System.out.println("Olá Mundo!" + (primeiro+segundo));
 */
+    }
+}
+class Livro {
+    private String nome;
+    private Integer numpag;
+
+    public Livro(String nome, Integer numpag) {
+        this.nome = nome;
+        this.numpag = numpag;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumpag() {
+        return numpag;
+    }
+
+    public void setNumpag(Integer numpag) {
+        this.numpag = numpag;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numpag=" + numpag +
+                '}';
     }
 }
